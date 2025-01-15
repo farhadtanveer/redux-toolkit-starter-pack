67250d430f0aaefe2706f916873d626873c160cf
farhadtanveer <farhadtanveer05@gmail.com>
2025-01-15 09:47:43 +0100

    redux toolkit starter pack to practice

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>Counter with redux</h1>
+      <button>Increament</button>
+      <div>0</div>
+      <button>Decreament</button>
+    </>
+  );
+}
+
+export default App;
